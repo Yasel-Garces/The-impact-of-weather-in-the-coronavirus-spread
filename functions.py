5e61d97781fc8d6da3afa6e5ce58cfb4baed1f28
Yasel-Garces <88yasel@gmail.com>
2020-04-14 13:17:46 -0400

    Finishing Pressure

--- functions.py
+++ functions.py
@@ -68,3 +68,41 @@ def factor_hum(val):
     else:
         return '>50'
 #--------------------------------------------------------
+def t_test_byCities(cities2test,df):
+    from scipy import stats
+    # Empty dictionary to store the results
+    results_pvalue=dict()
+    results_stat=dict()
+    # For each key in the dictionary, run the test with each of 
+    # the values of this key
+    for main_city in cities2test.keys():
+        # List with the cities to test with "hot_city"
+        paired_cities=cities2test[main_city]
+        # Extract the information of the "main_city"
+        main_city_values=df.loc[df['Province/State']==main_city,['Confirmed','Days Since First Case']]
+        # Define an empty dictiona ry to store the partial results 
+        p_value=dict()
+        stas_value=dict()
+        # Run the test for each pair of cities
+        for city in paired_cities:
+            # Extract the number of new cases of city
+            city_values=df.loc[df['Province/State']==city,['Confirmed','Days Since First Case']]
+            # Get the max number of observations available in both cities
+            max_mutual_obs=min(main_city_values.shape[0],city_values.shape[0])
+            # Store the information in X (key country) and Y (test country)
+            X=main_city_values.loc[main_city_values['Days Since First Case']<max_mutual_obs,'Confirmed']
+            Y=city_values.loc[city_values['Days Since First Case']<max_mutual_obs,'Confirmed']
+            # Run the t-student hypothesis test
+            stat, p = stats.ttest_ind(X, Y,equal_var=False)
+            # Save the p-value result in "partial". Because this is a one tail test, the p-value is divided by 2
+            p_value[city]=p/2
+            stas_value[city]=stat
+        # Include the dictionary with partial results in the general dictionary
+        results_pvalue[main_city]=p_value
+        results_stat[main_city]=stas_value
+    # Tranform the dictionary to a dataframe        
+    results_pvalue=pd.DataFrame.from_dict(results_pvalue, orient='columns')
+    results_stat=pd.DataFrame.from_dict(results_stat, orient='columns')
+    return (results_pvalue, results_stat)
+#--------------------------------------------------------        
+        
